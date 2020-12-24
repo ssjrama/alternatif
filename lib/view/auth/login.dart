793313c95e1367940ba755e14a177098bf489454
Wahyu Ramadhan <ssjrama@gmail.com>
2020-12-24 09:25:06 +0700

    Login & Register View

--- lib/view/auth/login.dart
+++ lib/view/auth/login.dart
@@ -1,39 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:alternatif/styles/styles.dart';
+import 'package:alternatif/widgets/styled_flat_button.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alternatif'),
-        centerTitle: true,
+        title: Text('Log In'),
+        leading: Container(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 200.0, 0, 0),
-        child: Column(
-          children: [
-            Center(
-              child: Text('Login'),
+      body: Center(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Log In',
+                    textAlign: TextAlign.center,
+                    style: Styles.h1,
+                  ),
+                  SizedBox(height: 10.0),
+                  SizedBox(height: 30.0),
+                  TextFormField(
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  StyledFlatButton(
+                    'Sign In',
+                  ),
+                  SizedBox(height: 20.0),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Belum punya akun ? ",
+                            style: Styles.p,
+                          ),
+                          TextSpan(
+                            text: 'Daftar.',
+                            style: Styles.p.copyWith(color: Colors.blue[500]),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => {
+                                    Navigator.pushNamed(context, '/register'),
+                                  },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                          text: 'Lupa Password ?',
+                          style: Styles.p.copyWith(color: Colors.blue[500]),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => {
+                                  Navigator.pushNamed(context, '/home'),
+                                }),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Text('Login'),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text('Register'),
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/view/auth/register.dart
+++ lib/view/auth/register.dart
@@ -1,30 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:alternatif/styles/styles.dart';
+import 'package:alternatif/widgets/styled_flat_button.dart';
 
 class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alternatif'),
-        centerTitle: true,
+        title: Text('Register'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 200.0, 0, 0),
-        child: Column(
-          children: [
-            Center(
-              child: Text('Register'),
+      body: Center(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Register Account',
+                    textAlign: TextAlign.center,
+                    style: Styles.h1,
+                  ),
+                  SizedBox(height: 10.0),
+                  SizedBox(height: 30.0),
+                  TextFormField(
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Name',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: Styles.input.copyWith(
+                      hintText: 'Password Confirm',
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  StyledFlatButton(
+                    'Register',
+                    //onPressed: Navigator.pushNamed(context, '/home'),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Text('Register'),
-            )
-          ],
+          ),
         ),
       ),
     );
